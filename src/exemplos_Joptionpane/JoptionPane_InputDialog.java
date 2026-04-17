6b8cc40c0627f8a966fc6172ebc1430de2fbede6
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-04-17 20:09:13 -0300

    Extras

--- src/exemplos_Joptionpane/JoptionPane_InputDialog.java
+++ src/exemplos_Joptionpane/JoptionPane_InputDialog.java
@@ -10,7 +10,11 @@ public class JoptionPane_InputDialog {
         String exemplo = JOptionPane.showInputDialog(null,"Escolhendo o que escrever em texto: ","Entrada de dados",JOptionPane.INFORMATION_MESSAGE);
         
         String exemplo02 = JOptionPane.showInputDialog(null,"Escolhendo o que escrever convertendo em INT: ","Entrada de dados",JOptionPane.INFORMATION_MESSAGE);
-        int exp2 = Integer.parseInt(exemplo02);
+        int expInt = Integer.parseInt(exemplo02);
+        //double expDouble = Double.parseDouble(exemplo02);
+        //boolean expBoolean = Boolean.parseBoolean(exemplo02);
+        //float expFloat = Float.parseFloat(exemplo02);
+        //String expString = String.valueOf(exemplo02);
         
         int exemplo03 = Integer.parseInt(JOptionPane.showInputDialog(null,"Escolhendo o que escrever convertendo em INT: ","Entrada de dados",JOptionPane.INFORMATION_MESSAGE));
     }  
--- src/exemplos_Joptionpane/JoptionPane_InputDialogListagem.java
+++ src/exemplos_Joptionpane/JoptionPane_InputDialogListagem.java
@@ -7,7 +7,6 @@ public class JoptionPane_InputDialogListagem {
     
         public static void main(String[] args) {
             
-            
             //showOptionDialog em Vetor variavel Object (INT)
             Object[] variavel = {"Antes","Agora","Depois"};
             Object escolher = JOptionPane.showInputDialog(null,"Escolha um dos itens: ","Caixa de itens",JOptionPane.QUESTION_MESSAGE,null,variavel,variavel[0]);
