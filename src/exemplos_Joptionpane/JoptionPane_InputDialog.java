2fe6fb5847f79602ec3a5244612afcdc4876a772
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-05-15 00:33:30 -0300

    nada

--- src/exemplos_Joptionpane/JoptionPane_InputDialog.java
+++ src/exemplos_Joptionpane/JoptionPane_InputDialog.java
@@ -1,10 +1,12 @@
 
 package exemplos_Joptionpane;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class JoptionPane_InputDialog {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         
         //showInputDialog tem variavel em String com converções INT
         String exemplo = JOptionPane.showInputDialog(null,"Escolhendo o que escrever em texto: ","Entrada de dados",JOptionPane.INFORMATION_MESSAGE);
@@ -17,5 +19,35 @@ public class JoptionPane_InputDialog {
         //String expString = String.valueOf(exemplo02);
         
         int exemplo03 = Integer.parseInt(JOptionPane.showInputDialog(null,"Escolhendo o que escrever convertendo em INT: ","Entrada de dados",JOptionPane.INFORMATION_MESSAGE));
+            
+        //Exemplo de comparação para fazer Joptionpane:
+        
+        System.out.println("Mensagem comum");
+        
+        JOptionPane.showMessageDialog(null,"Mensagem comum");
+        
+        
+        //Exemplo de mensagem com entrada:
+
+        System.out.println("Digite seu nome:");
+        String nomeAA = scan.next();
+      
+        String nomeA = JOptionPane.showInputDialog(null,"Mensagem comum");
+        
+        
+        //Exemplo de pergunta sim ou não:
+
+        System.out.println("Deseja continuar ?");
+        int algoAA = scan.nextInt();
+        
+        int algoA = JOptionPane.showConfirmDialog(null,"Deseja continuar ?");
+    
+        
+        //Exemplo
+        
+    
+    
+    
+    
     }  
 }
